858e7adde29ae6e34ab43c6c6635b3e1af2a4393
Johan Brinch <brinchj@gmail.com>
2011-08-11 18:40:03 +0200

    fixes

--- broker.py
+++ broker.py
@@ -13,17 +13,21 @@ def maybeRetry(action):
 BTC_FACTOR = 10**8
 USD_FACTOR = 10**5
 
+def to_decimal(flt):
+    return Decimal(str(flt))
+
 def intBTC(usd):
-    return int(Decimal(usd) * BTC_FACTOR)
+    return int(to_decimal(usd) * BTC_FACTOR)
 
 def intUSD(usd):
-    return int(Decimal(usd) * USD_FACTOR)
+    return int(to_decimal(usd) * USD_FACTOR)
 
 def floatBTC(btc):
-    return str(Decimal(btc) / BTC_FACTOR)
+    return str(to_decimal(btc) / BTC_FACTOR)
 
 def floatUSD(usd):
-    return str(Decimal(usd) / USD_FACTOR)
+    return str(to_decimal(usd) / USD_FACTOR)
+
 
 class Broker(Thread):
     def __init__(self, gox):
@@ -53,7 +57,8 @@ class Broker(Thread):
             bids = data['bids'][-1::-1]
         else:
             bids = data['asks']
-        bids = map(lambda x: (intUSD(x[0]), intBTC(x[1])), bids)
+
+        bids = map(lambda (usd,btc): (intUSD(usd), intBTC(btc)), bids)
         n = 0
         for (p, a) in bids:
             n += a
